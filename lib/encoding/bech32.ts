0bc1605b8732711c59b4eaf138f9b9074fb1e6de
y-chan <y-chan@y-chan.dev>
2021-03-14 21:30:44 +0900

    rewrite lib encoding bech32

--- lib/encoding/bech32.ts
+++ lib/encoding/bech32.ts
@@ -1,24 +1,24 @@
 const CHARSET = 'qpzry9x8gf2tvdw0s3jn54khce6mua7l'
 const GENERATOR = [0x3b6a57b2, 0x26508e6d, 0x1ea119fa, 0x3d4233dd, 0x2a1462b3]
 
-class InvalidBech32StringError extends Error {
-  constructor(...args) {
+export class InvalidBech32StringError extends Error {
+  constructor(...args: string[]) {
     super(...args)
     Error.captureStackTrace(this, this.constructor)
   }
 
-  get name() {
+  get name(): string {
     return this.constructor.name
   }
 }
 
-function polymod(values) {
-  let mod = 1
+function polymod(values: number[]): number {
+  let mod: number = 1
   for (let x of values) {
-    let top = mod >>> 25
-    mod = (mod & 0x1ffffff) << 5 | x
+    let top: number = mod >>> 25
+    mod = ((mod & 0x1ffffff) << 5) | x
     for (let i = 0; i < 5; ++i) {
-      if (top >>> i & 1) {
+      if ((top >>> i) & 1) {
         mod ^= GENERATOR[i]
       }
     }
@@ -26,8 +26,8 @@ function polymod(values) {
   return mod
 }
 
-function hrpExpand(hrp) {
-  let result = []
+function hrpExpand(hrp: string): number[] {
+  let result: number[] = []
   for (let p = 0; p < hrp.length; ++p) {
     result.push(hrp.charCodeAt(p) >>> 5)
   }
@@ -38,30 +38,30 @@ function hrpExpand(hrp) {
   return result
 }
 
-function verifyChecksum(hrp, data) {
+function verifyChecksum(hrp: string, data: number[]): boolean {
   return polymod(hrpExpand(hrp).concat(data)) === 1
 }
 
-function createChecksum(hrp, data) {
-  let values = [...hrpExpand(hrp), ...data, 0, 0, 0, 0, 0, 0]
-  let mod = polymod(values) ^ 1
-  let result = []
+function createChecksum(hrp: string, data: number[]): number[] {
+  let values: number[] = [...hrpExpand(hrp), ...data, 0, 0, 0, 0, 0, 0]
+  let mod: number = polymod(values) ^ 1
+  let result: number[] = []
   for (let p = 0; p < 6; ++p) {
-    result.push(mod >>> 5 * (5 - p) & 31)
+    result.push((mod >>> (5 * (5 - p))) & 31)
   }
   return result
 }
 
-function encode(hrp, data) {
-  let combined = data.concat(createChecksum(hrp, data))
-  return `${hrp}1${combined.map(s => CHARSET[s]).join('')}`
+export function encode(hrp: string, data: number[]): string {
+  let combined: number[] = data.concat(createChecksum(hrp, data))
+  return `${hrp}1${combined.map((s: number) => CHARSET[s]).join('')}`
 }
 
-function decode(bechString) {
-  let hasLower = false
-  let hasUpper = false
+export function decode(bechString: string) {
+  let hasLower: boolean = false
+  let hasUpper: boolean = false
   for (let p = 0; p < bechString.length; ++p) {
-    let code = bechString.charCodeAt(p)
+    let code: number = bechString.charCodeAt(p)
     if (code < 33 || code > 126) {
       throw new InvalidBech32StringError(bechString)
     }
@@ -76,12 +76,16 @@ function decode(bechString) {
     throw new InvalidBech32StringError(bechString)
   }
   bechString = bechString.toLowerCase()
-  let position = bechString.lastIndexOf('1')
-  if (position < 1 || position + 7 > bechString.length || bechString.length > 90) {
+  let position: number = bechString.lastIndexOf('1')
+  if (
+    position < 1 ||
+    position + 7 > bechString.length ||
+    bechString.length > 90
+  ) {
     throw new InvalidBech32StringError(bechString)
   }
-  let hrp = bechString.slice(0, position)
-  let data = []
+  let hrp: string = bechString.slice(0, position)
+  let data: number[] = []
   for (let s of bechString.slice(position + 1)) {
     let d = CHARSET.indexOf(s)
     if (d === -1) {
@@ -92,7 +96,5 @@ function decode(bechString) {
   if (!verifyChecksum(hrp, data)) {
     throw new InvalidBech32StringError(bechString)
   }
-  return {hrp, data: data.slice(0, data.length - 6)}
+  return { hrp, data: data.slice(0, data.length - 6) }
 }
-
-Object.assign(exports, {encode, decode, InvalidBech32StringError})
