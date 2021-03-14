07aac3b7a3723371021968aaa901c8ffd15859d1
y-chan <y-chan@y-chan.dev>
2021-03-14 21:19:26 +0900

    add return types

--- lib/crypto/hash.ts
+++ lib/crypto/hash.ts
@@ -1,17 +1,17 @@
 import { BinaryLike, createHash } from 'crypto'
 
-export function sha256(buffer: BinaryLike) {
+export function sha256(buffer: BinaryLike): Buffer {
   return createHash('sha256').update(buffer).digest()
 }
 
-export function sha256d(buffer: BinaryLike) {
+export function sha256d(buffer: BinaryLike): Buffer {
   return sha256(sha256(buffer))
 }
 
-function ripemd160(buffer: BinaryLike) {
+function ripemd160(buffer: BinaryLike): Buffer {
   return createHash('ripemd160').update(buffer).digest()
 }
 
-export function hash160(buffer: BinaryLike) {
+export function hash160(buffer: BinaryLike): Buffer {
   return ripemd160(sha256(buffer))
 }
