5e2525febdd748a383963182cd7d9aa611a96f0d
y-chan <y-chan@y-chan.dev>
2021-03-19 15:38:07 +0900

    rewrite node models tip

---
+++ node/models/tip.ts
@@ -0,0 +1,20 @@
+import {
+  Table,
+  Column,
+  Model,
+  PrimaryKey,
+  DataType,
+} from 'sequelize-typescript'
+
+@Table({ freezeTableName: true, underscored: true, timestamps: false })
+export default class Tip extends Model<Tip> {
+  @PrimaryKey
+  @Column(DataType.STRING)
+  service!: string
+
+  @Column(DataType.INTEGER.UNSIGNED)
+  height!: number
+
+  @Column(DataType.STRING(32).BINARY)
+  hash!: Buffer
+}
