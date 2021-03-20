7b45059009c11ce0a8612c306a45e98aede1e97d
y-chan <y-chan@y-chan.dev>
2021-03-21 03:48:42 +0900

    rewrite node models transaction-output

---
+++ node/models/transaction-output.ts
@@ -0,0 +1,110 @@
+import {
+  Table,
+  Column,
+  Model,
+  PrimaryKey,
+  AllowNull,
+  DataType,
+  BelongsTo,
+  ForeignKey,
+} from 'sequelize-typescript'
+import { Transaction } from '@/node/models/transaction'
+import Address from '~/node/models/address'
+
+@Table({ freezeTableName: true, underscored: true, timestamps: false })
+export class TransactionOutput extends Model<TransactionOutput> {
+  @PrimaryKey
+  @ForeignKey(() => Transaction)
+  @Column(DataType.BIGINT.UNSIGNED)
+  transactionId!: bigint
+
+  @PrimaryKey
+  @Column(DataType.INTEGER.UNSIGNED)
+  outputIndex!: number
+
+  @Column({ type: DataType.BLOB('medium'), field: 'scriptpubkey' })
+  scriptPubkey!: Buffer
+
+  @Column(DataType.INTEGER.UNSIGNED)
+  blockHeight!: number
+
+  @Column(DataType.BIGINT)
+  value!: bigint
+
+  @Column(DataType.BIGINT.UNSIGNED)
+  addressId!: bigint
+
+  @Column(DataType.BOOLEAN)
+  isStake!: boolean
+
+  @Column(DataType.BIGINT.UNSIGNED)
+  inputId!: bigint
+
+  @Column(DataType.INTEGER.UNSIGNED)
+  inputIndex!: number
+
+  @AllowNull
+  @Column(DataType.INTEGER.UNSIGNED)
+  inputHeight!: number
+
+  @BelongsTo(() => Transaction)
+  transaction!: Transaction
+}
+
+@Table({ freezeTableName: true, underscored: true, timestamps: false })
+export class TransactionInput extends Model<TransactionInput> {
+  @PrimaryKey
+  @ForeignKey(() => Transaction)
+  @Column(DataType.BIGINT.UNSIGNED)
+  transactionId!: bigint
+
+  @PrimaryKey
+  @Column(DataType.INTEGER.UNSIGNED)
+  inputIndex!: number
+
+  @Column({ type: DataType.BLOB('medium'), field: 'scriptsig' })
+  scriptSig!: Buffer
+
+  @Column(DataType.INTEGER.UNSIGNED)
+  sequence!: number
+
+  @Column(DataType.INTEGER.UNSIGNED)
+  blockHeight!: number
+
+  @Column(DataType.BIGINT)
+  value!: bigint
+
+  @ForeignKey(() => Address)
+  @Column(DataType.BIGINT.UNSIGNED)
+  addressId!: bigint
+
+  @Column(DataType.BIGINT.UNSIGNED)
+  outputId!: bigint
+
+  @Column(DataType.INTEGER.UNSIGNED)
+  outputIndex!: number
+
+  @BelongsTo(() => Transaction)
+  transaction!: Transaction
+
+  @BelongsTo(() => Address)
+  address!: Address
+}
+
+@Table({ freezeTableName: true, underscored: true, timestamps: false })
+export class TransactionOutputMapping extends Model<TransactionOutputMapping> {
+  @Column({ type: DataType.STRING(32), field: '_id' })
+  _id!: string
+
+  @Column({ type: DataType.STRING(32).BINARY, field: 'input_transaction_id' })
+  inputTxId!: Buffer
+
+  @Column(DataType.INTEGER.UNSIGNED)
+  inputIndex!: number
+
+  @Column({ type: DataType.STRING(32).BINARY, field: 'output_transaction_id' })
+  outputTxId!: Buffer
+
+  @Column(DataType.INTEGER.UNSIGNED)
+  outputIndex!: number
+}
