921eef7766e80cf9cd60a62a1863aa70bc007c4a
y-chan <y-chan@y-chan.dev>
2021-03-24 00:43:45 +0900

    rewrite p2p commands commands getaddr

--- p2p/commands/commands/getaddr.js
+++
@@ -1,9 +0,0 @@
-const Message = require('./message')
-
-class GetAddrMessage extends Message {
-  constructor(options) {
-    super('getaddr', options)
-  }
-}
-
-module.exports = GetAddrMessage
---
+++ p2p/commands/commands/getaddr.ts
@@ -0,0 +1,15 @@
+import Message, { MessageOptions } from '@/p2p/commands/commands/message'
+
+class GetAddrMessage extends Message {
+  constructor(options: MessageOptions) {
+    super('getaddr', options)
+  }
+
+  static fromBuffer(payload: Buffer, options: MessageOptions) {
+    let message = new GetAddrMessage(options)
+    message.payload = payload
+    return message
+  }
+}
+
+export default GetAddrMessage
