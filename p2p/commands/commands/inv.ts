6ca63e4af2c8686d538651e45d3c3b20a2a306ad
y-chan <y-chan@y-chan.dev>
2021-03-24 01:44:48 +0900

    rewrite p2p commands commands inv

---
+++ p2p/commands/commands/inv.ts
@@ -0,0 +1,46 @@
+import BufferReader from '@/lib/encoding/buffer-reader'
+import BufferWriter from '@/lib/encoding/buffer-writer'
+import Message, {
+  IMessage,
+  MessageOptions,
+} from '@/p2p/commands/commands/message'
+import {
+  Inventory,
+  parseInventories,
+  writeInventories,
+} from '@/p2p/commands/commands/utils'
+
+export interface InvMessageOptions extends MessageOptions {
+  inventories: Inventory[]
+}
+
+export interface IInvMessage extends InvMessageOptions, IMessage {}
+
+class InvMessage extends Message implements IInvMessage {
+  public inventories: Inventory[]
+
+  constructor({ inventories, ...options }: InvMessageOptions) {
+    super('inv', options)
+    this.inventories = inventories
+  }
+
+  static fromBuffer(payload: Buffer, options: InvMessageOptions) {
+    let message = new InvMessage(options)
+    message.payload = payload
+    return message
+  }
+
+  get payload(): Buffer {
+    let writer = new BufferWriter()
+    writeInventories(writer, this.inventories)
+    return writer.toBuffer()
+  }
+
+  set payload(payload: Buffer) {
+    let reader = new BufferReader(payload)
+    this.inventories = parseInventories(reader)
+    Message.checkFinished(reader)
+  }
+}
+
+export default InvMessage
