f62effe13fb031302a0959b6b0a32c71cc861cb1
y-chan <y-chan@y-chan.dev>
2021-03-24 00:35:15 +0900

    rewrite p2p commands commands block

---
+++ p2p/commands/commands/block.ts
@@ -0,0 +1,29 @@
+import Block, { IBlock } from '@/lib/block/block'
+import Message, {
+  IMessage,
+  MessageOptions,
+} from '@/p2p/commands/commands/message'
+
+export interface BlockMessageOptions extends MessageOptions {
+  block: IBlock
+}
+
+export interface IBlockMessage extends BlockMessageOptions, IMessage {}
+
+class BlockMessage extends Message implements IBlockMessage {
+  public block: IBlock
+  constructor({ block, ...options }: BlockMessageOptions) {
+    super('block', options)
+    this.block = block
+  }
+
+  get payload(): Buffer {
+    return this.block.toBuffer()
+  }
+
+  set payload(payload: Buffer) {
+    this.block = Block.fromBuffer(payload)
+  }
+}
+
+export default BlockMessage
