f1bf5aa0647d2cf2cd87c6eef1d8b6666a983250
y-chan <y-chan@y-chan.dev>
2021-03-24 01:25:06 +0900

    rewrite p2p commands commands getdata

---
+++ p2p/commands/commands/getdata.ts
@@ -0,0 +1,43 @@
+import BufferReader from '@/lib/encoding/buffer-reader'
+import BufferWriter from '@/lib/encoding/buffer-writer'
+import Message, {IMessage, MessageOptions} from '@/p2p/commands/commands/message'
+import {
+  Inventory,
+  parseInventories,
+  writeInventories,
+} from '@/p2p/commands/commands/utils'
+
+export interface GetDataMessageOptions extends MessageOptions {
+  inventories: Inventory[]
+}
+
+export interface IGetDataMessage extends GetDataMessageOptions, IMessage {}
+
+class GetDataMessage extends Message implements IGetDataMessage {
+  public inventories: Inventory[]
+
+  constructor({ inventories, ...options }: GetDataMessageOptions) {
+    super('getdata', options)
+    this.inventories = inventories
+  }
+
+  static fromBuffer(payload: Buffer, options: GetDataMessageOptions) {
+    let message = new GetDataMessage(options)
+    message.payload = payload
+    return message
+  }
+
+  get payload(): Buffer {
+    let writer = new BufferWriter()
+    writeInventories(writer, this.inventories)
+    return writer.toBuffer()
+  }
+
+  set payload(payload: Buffer) {
+    let reader = new BufferReader(payload)
+    this.inventories = parseInventories(reader)
+    Message.checkFinished(reader)
+  }
+}
+
+export default GetDataMessage
