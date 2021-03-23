e4571fda40d7f7b1f8a9a952db0b84a05ff271dd
y-chan <y-chan@y-chan.dev>
2021-03-24 01:59:48 +0900

    rewrite p2p commands commands pong

---
+++ p2p/commands/commands/pong.ts
@@ -0,0 +1,42 @@
+import BufferReader from '@/lib/encoding/buffer-reader'
+import Message, {
+  IMessage,
+  MessageOptions,
+} from '@/p2p/commands/commands/message'
+import { getNonce } from '@/p2p/commands/commands/utils'
+
+export interface PongMessageOptions extends MessageOptions {
+  nonce: Buffer
+}
+
+export interface IPongMessage extends PongMessageOptions, IMessage {}
+
+class PongMessage extends Message implements IPongMessage {
+  public nonce: Buffer
+
+  constructor({ nonce = getNonce(), ...options }: PongMessageOptions) {
+    super('pong', options)
+    this.nonce = nonce
+  }
+
+  static fromBuffer(payload: Buffer, options: PongMessageOptions): PongMessage {
+    let message = new PongMessage(options)
+    message.payload = payload
+    return message
+  }
+
+  get payload(): Buffer {
+    return this.nonce
+  }
+
+  set payload(payload) {
+    let reader = new BufferReader(payload)
+    let nonce = reader.read(8)
+    if (nonce) {
+      this.nonce = nonce
+    }
+    Message.checkFinished(reader)
+  }
+}
+
+export default PongMessage
