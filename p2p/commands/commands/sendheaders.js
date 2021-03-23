6601f4b79fd7e4b76d8205c058c5f7801bf42611
y-chan <y-chan@y-chan.dev>
2021-03-24 02:30:24 +0900

    rewrite p2p commands commands sendheaders

--- p2p/commands/commands/sendheaders.js
+++
@@ -1,9 +0,0 @@
-const Message = require('./message')
-
-class SendHeadersMessage extends Message {
-  constructor(options) {
-    super('sendheaders', options)
-  }
-}
-
-module.exports = SendHeadersMessage
---
+++ p2p/commands/commands/sendheaders.ts
@@ -0,0 +1,18 @@
+import Message, { MessageOptions } from '@/p2p/commands/commands/message'
+
+class SendHeadersMessage extends Message {
+  constructor(options: MessageOptions) {
+    super('sendheaders', options)
+  }
+
+  static fromBuffer(
+    payload: Buffer,
+    options: MessageOptions
+  ): SendHeadersMessage {
+    let message = new SendHeadersMessage(options)
+    message.payload = payload
+    return message
+  }
+}
+
+export default SendHeadersMessage
