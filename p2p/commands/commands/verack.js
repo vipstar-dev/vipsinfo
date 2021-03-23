ba9a11541c07607c5b92c35712de98567adadd24
y-chan <y-chan@y-chan.dev>
2021-03-24 02:39:01 +0900

    rewrite p2p commands commands verack

--- p2p/commands/commands/verack.js
+++
@@ -1,9 +0,0 @@
-const Message = require('./message')
-
-class VerackMessage extends Message {
-  constructor(options) {
-    super('verack', options)
-  }
-}
-
-module.exports = VerackMessage
---
+++ p2p/commands/commands/verack.ts
@@ -0,0 +1,15 @@
+import Message, { MessageOptions } from '@/p2p/commands/commands/message'
+
+class VerackMessage extends Message {
+  constructor(options: MessageOptions) {
+    super('verack', options)
+  }
+
+  static fromBuffer(payload: Buffer, options: MessageOptions): VerackMessage {
+    let message = new VerackMessage(options)
+    message.payload = payload
+    return message
+  }
+}
+
+export default VerackMessage
