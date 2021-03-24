bf2864171ce458729234d7488fa3a808f69a2270
y-chan <y-chan@y-chan.dev>
2021-03-24 23:50:49 +0900

    rewrite p2p index

--- p2p/index.js
+++
@@ -1,6 +0,0 @@
-const Pool = require('./pool')
-const Peer = require('./peer')
-const Messages = require('./commands/messages')
-const Inventory = require('./commands/inventory')
-
-Object.assign(exports, {Pool, Peer, Messages, Inventory})
---
+++ p2p/index.ts
@@ -0,0 +1,4 @@
+export * from '@/p2p/commands/inventory'
+export * from '@/p2p/commands/messages'
+export * from '@/p2p/peer'
+export * from '@/p2p/pool'
