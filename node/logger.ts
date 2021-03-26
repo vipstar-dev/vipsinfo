83a678f42243c4f27f05e484795447135c28a073
y-chan <y-chan@y-chan.dev>
2021-03-27 03:03:11 +0900

    fix logger (use any array)

--- node/logger.ts
+++ node/logger.ts
@@ -9,15 +9,11 @@ type levelTypes = 'info' | 'error' | 'debug' | 'warn'
 
 export interface ILogger extends LoggerConstructor {
   formatting: boolean
-  info(...args: (number | string | undefined)[]): void
-  warn(...args: (number | string | undefined)[]): void
-  error(...args: (number | string | undefined)[]): void
-  debug(...args: (number | string | undefined)[]): void
-  _log(
-    color: colorTypes,
-    level: levelTypes,
-    ...args: (number | string | undefined)[]
-  ): void
+  info(...args: any[]): void
+  warn(...args: any[]): void
+  error(...args: any[]): void
+  debug(...args: any[]): void
+  _log(color: colorTypes, level: levelTypes, ...args: any[]): void
 }
 
 class Logger implements ILogger {
@@ -27,29 +23,25 @@ class Logger implements ILogger {
     this.formatting = formatting
   }
 
-  info(...args: (number | string | undefined)[]): void {
+  info(...args: any[]): void {
     this._log('blue', 'info', ...args)
   }
 
-  error(...args: (number | string | undefined)[]): void {
+  error(...args: any[]): void {
     this._log('red', 'error', ...args)
   }
 
-  debug(...args: (number | string | undefined)[]): void {
+  debug(...args: any[]): void {
     if (process.env.QTUMINFO_ENV === 'debug') {
       this._log('green', 'debug', ...args)
     }
   }
 
-  warn(...args: (number | string | undefined)[]): void {
+  warn(...args: any[]): void {
     this._log('yellow', 'warn', ...args)
   }
 
-  _log(
-    color: colorTypes,
-    level: levelTypes,
-    ...args: (number | string | undefined)[]
-  ): void {
+  _log(color: colorTypes, level: levelTypes, ...args: any[]): void {
     if (this.formatting) {
       let date = new Date()
       let typeString = colors[color](`${level}:`)
