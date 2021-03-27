a19bce1a635604b1a31850d1dd30f42e69e3f9ef
y-chan <y-chan@y-chan.dev>
2021-03-28 02:39:47 +0900

    return types disambiguate

--- lib/script/output.ts
+++ lib/script/output.ts
@@ -106,7 +106,22 @@ export interface IContractOutputScript extends IOutputScript {
 }
 
 class OutputScript extends Script implements IOutputScript {
-  static fromBuffer(buffer: Buffer): OutputScript {
+  static fromBuffer(
+    buffer: Buffer
+  ):
+    | IOutputScript
+    | IDataOutputScript
+    | IPublicKeyOutputScript
+    | IPublicKeyHashOutputScript
+    | IScriptHashOutputScript
+    | IMultisigOutputScript
+    | IWitnessV0KeyHashOutputScript
+    | IWitnessV0ScriptHashOut
+    | IEVMContractCreateScript
+    | IEVMContractCreateBySenderScript
+    | IEVMContractCallScript
+    | IEVMContractCallBySenderScript
+    | IContractOutputScript {
     if (buffer[0] === Opcode.OP_RETURN) {
       return new DataOutputScript([
         { code: Opcode.OP_RETURN, buffer: buffer.slice(1) },
