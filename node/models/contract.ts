330e411a3f48dcb69aa54dc4cb2a9c564613f9eb
y-chan <y-chan@y-chan.dev>
2021-03-29 02:27:15 +0900

    fix miss(remove type from contract creation attributes)

--- node/models/contract.ts
+++ node/models/contract.ts
@@ -36,6 +36,7 @@ export interface ContractModelAttributes {
 export interface ContractCreationAttributes
   extends Optional<
     ContractModelAttributes,
+    | 'type'
     | 'description'
     | 'code'
     | 'tag'
