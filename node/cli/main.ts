0b37bd4a0fdfe666652d3107061701e311fe5d64
y-chan <y-chan@y-chan.dev>
2021-03-29 05:26:53 +0900

    rewrite node cli main

---
+++ node/cli/main.ts
@@ -0,0 +1,54 @@
+import program from 'commander'
+import Liftoff from 'liftoff'
+import path from 'path'
+
+import VipsNode from '@/node/cli/node'
+import { ConfigFile } from '@/node/node'
+import packageJson from '@/package.json'
+
+process.on('unhandledRejection', (reason: object | null | undefined) =>
+  console.error(reason)
+)
+
+let liftoff = new Liftoff({
+  name: 'vipsinfo',
+  moduleName: 'vipsinfo-node',
+  configName: 'vipsinfo-node',
+  processTitle: 'vipsinfo',
+})
+  .on('require', (name) => {
+    console.log('Loading:', name)
+  })
+  .on('requireFail', (name, err) => {
+    console.error('Unable to load:', name, err)
+  })
+  .on('respawn', (flags, child) => {
+    console.log('Detected node flags:', flags)
+    console.log('Respawned to PID', child.pid)
+  })
+
+liftoff.launch({ cwd: process.cwd() }, () => {
+  program.version(packageJson.version)
+
+  program
+    .command('start')
+    .description('Start the current node')
+    .option(
+      '-c, --config <dir>',
+      'Specify the directory with Vipsinfo Node configuration'
+    )
+    .action(async (cmd: { [key: string]: string }) => {
+      let config: ConfigFile = require(path.resolve(
+        process.cwd(),
+        ...(cmd.config ? [cmd.config] : []),
+        'vipsinfo-node.json'
+      )) as ConfigFile
+      let node = new VipsNode({ path: process.cwd(), config })
+      await node.start()
+    })
+
+  program.parse(process.argv)
+  if (process.argv.length === 2) {
+    program.help()
+  }
+})
